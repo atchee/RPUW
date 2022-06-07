4c569d3578b013249209584be827c04318725550
adams laurent <adams.khalfi@gmail.com>
2022-06-07 17:36:13 +0200

    fixed

--- app/javascript/listener.js
+++
@@ -1,9 +0,0 @@
-const element = document.querySelector("#clickme");
-console.log(element)
-element.addEventListener("click", (event) => {
-
-  console.log(element)
-  // Do something (callback)
-  const audio = new Audio('https://res.cloudinary.com/alk1805/video/upload/v1654597546/RPUW/ouioui.mp3_kv3pxq.mp3');
-  event.currentTarget = audio.play();
-});
